105914e6ed7a5294a7baaac341c234cd412188a0
Diego Bejarano <diegobejaranot@gmail.com>
2024-10-16 17:56:58 -0500

    auth anonima added

--- BejaranoSnapchat/iniciarSesionViewController.swift
+++ BejaranoSnapchat/iniciarSesionViewController.swift
@@ -19,13 +19,31 @@ class iniciarSesionViewController: UIViewController {
     }
     
     @IBAction func iniciarSesionTapped(_ sender: Any) {
-        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
-            (user, error) in
-            print("Intentando Iniciar Sesión")
-            if error != nil {
-                print("Se presentó el siguiente error: \(error)")
-            } else {
-                print("Inicio de sesión exitoso")
+//        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+//            (user, error) in
+//            print("Intentando Iniciar Sesión")
+//            if error != nil {
+//                print("Se presentó el siguiente error: \(error)")
+//            } else {
+//                print("Inicio de sesión exitoso")
+//            }
+//        }
+        
+        Auth.auth().signInAnonymously { authResult, error in
+            if let error = error {
+                // Maneja el error si ocurre
+                print("Error al iniciar sesión anónima: \(error.localizedDescription)")
+                return
+            }
+            
+            // Si el inicio de sesión es exitoso, puedes acceder al usuario autenticado
+            if let user = authResult?.user {
+                guard let user = authResult?.user else { return } // true
+                let uid = user.uid
+                print("Inicio de sesión anónimo exitoso. ID del usuario: \(uid)")
+                
+                // Puedes guardar el ID del usuario, realizar otras acciones necesarias,
+                // como navegar a otra pantalla o mostrar información al usuario.
             }
         }
     }
